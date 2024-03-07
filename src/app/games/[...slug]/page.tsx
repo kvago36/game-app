6779b0a7637079ac285dd98ec865a21417b23cf3
Alexander Kosarchuk <kvago36@gmail.com>
2024-03-07 19:12:54 +0700

    remove json

--- src/app/games/[...slug]/page.tsx
+++ src/app/games/[...slug]/page.tsx
@@ -1,11 +1,11 @@
-import { notFound } from "next/navigation"
+import { notFound } from "next/navigation";
 import Image from "next/image";
-import type { Metadata, ResolvingMetadata } from 'next'
+import type { Metadata, ResolvingMetadata } from "next";
 
 interface PageProps {
   params: {
-    slug: string[]
-  }
+    slug: string[];
+  };
 }
 
 type Game = {
@@ -17,72 +17,84 @@ type Game = {
 };
 
 async function getPageFromParams({ params }: PageProps) {
-  const [id, seo_title] = params.slug
-  const posts: Game[] = await fetch('https://nextjs-test-pi-hazel-56.vercel.app/data/games.json').then((res) => res.json())
-  const postByProvider = posts.find(post => post.seo_title === seo_title && post.provider === id)
+  const [id, seo_title] = params.slug;
+  const posts: Game[] = await fetch(
+    "https://nextjs-test-pi-hazel-56.vercel.app/data/games.json"
+  ).then((res) => res.json());
+  const postByProvider = posts.find(
+    (post) => post.seo_title === seo_title && post.provider === id
+  );
 
   if (postByProvider) {
-    return postByProvider
+    return postByProvider;
   }
 
-  return posts.find(post => post.seo_title === seo_title && post.categories.includes(id))
+  return posts.find(
+    (post) => post.seo_title === seo_title && post.categories.includes(id)
+  );
 }
 
-export async function generateMetadata(
-  { params }: PageProps,
-): Promise<Metadata> {
-  const post = await getPageFromParams({ params })
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
+  const post = await getPageFromParams({ params });
 
   if (!post) {
     return {
-      title: '404',
-    }
+      title: "404",
+    };
   }
 
   return {
     title: post.title,
-  }
+  };
 }
 
 export async function generateStaticParams() {
-  const posts: Game[] = await fetch('https://nextjs-test-pi-hazel-56.vercel.app/data/games.json').then((res) => res.json())
+  const posts: Game[] = await fetch(
+    "https://nextjs-test-pi-hazel-56.vercel.app/data/games.json"
+  ).then((res) => res.json());
 
-  const postsByProvider = posts.map(post => ({
+  const postsByProvider = posts.map((post) => ({
     slug: [post.provider, post.seo_title],
-  }))
+  }));
 
-  const postsByCategory = posts.flatMap(post => (
-    post.categories.map(category => ({
-      slug: [category, post.seo_title]
+  const postsByCategory = posts.flatMap((post) =>
+    post.categories.map((category) => ({
+      slug: [category, post.seo_title],
     }))
-  ))
+  );
 
-  return postsByProvider.concat(postsByCategory)
+  return postsByProvider.concat(postsByCategory);
 }
- 
+
 // Multiple versions of this page will be statically generated
 // using the `params` returned by `generateStaticParams`
 export default async function Page({ params }: PageProps) {
-  const page = await getPageFromParams({ params })
-  
+  const page = await getPageFromParams({ params });
+
   if (!page) {
-    notFound()
+    notFound();
   }
 
   return (
-    <article className="prose lg:prose-xl">
-      <h1>{page.title}</h1>
-      <h4>Provider - {page.provider}</h4>
-      <Image
-        src={`https://d2norla3tyc4cn.cloudfront.net/i/s3/${page.identifier}.webp`}
-        width={500}
-        height={500}
-        alt="Picture of the author"
-      />
-      <h4>Categories:</h4>
-      <ul>
-        {page.categories.map(category => <li key={category}>{category}</li>)}
-      </ul>
-    </article>
-  )
+    <div className="relative w-full px-6 py-12 bg-white shadow-xl shadow-slate-700/10 ring-1 ring-gray-900/5 md:max-w-3xl md:mx-auto lg:max-w-4xl lg:pt-16 lg:pb-28">
+      <article className="prose lg:prose-xl">
+        <h1>{page.title}</h1>
+        <h4>Provider - {page.provider}</h4>
+        <Image
+          src={`https://d2norla3tyc4cn.cloudfront.net/i/s3/${page.identifier}.webp`}
+          width={500}
+          height={500}
+          alt="Picture of the author"
+        />
+        <h4>Categories:</h4>
+        <ul>
+          {page.categories.map((category) => (
+            <li key={category}>{category}</li>
+          ))}
+        </ul>
+      </article>
+    </div>
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="relative w-full px-6 py-12 bg-white shadow-xl shadow-slate-700/10 ring-1 ring-gray-900/5 md:max-w-3xl md:mx-auto lg:max-w-4xl lg:pt-16 lg:pb-28">
+      <body>
         {children}
       </body>
     </html>
